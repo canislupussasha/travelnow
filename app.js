77b41c96f2fe113a86b7e0dfd8241f0e33d73e7c
Merve Tuz <mervetuz11@gmail.com>
2022-06-15 08:31:34 +0200

    mysql connection

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
  //we need to import Express that we just installed to make sure that we can start our server from nodejs
 const express = require('express');
 
+//import mysql
+const mysql = require('mysql');
+
 //start a server with app
 const app = express();
+
+//create all the values for the connection to the database
+const db = mysql.createConnection({
+    //you need to tell which one is the host, we are running local
+    host: 'localhost',
+    //we use XAMPP and default user is root
+    user: 'root',
+    password: '',
+    //we have create a new database 'nodejs-login' phpMyAdmin
+    database: 'nodejs-login'
+
+});
+//we need to connect to the database
+//the function can have an error or result
+db.connect((error) => {
+
+    if(error){
+        console.log(error);
+    }else{
+        console.log("MYSQL Connected...")
+    }
+
+})
+
 
 //request:  grab something from a form for example 
 //response is what you want to send to the frontend
