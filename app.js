cad568154fb0a8078118f824aea0885afb16ec02
AlexanderTomic <mirela.tomic35@gmail.com>
2022-06-15 22:57:46 +0200

    php

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-//we need to import Express that we just installed to make sure that we can start our server from nodejs
+
+const { decodeBase64 } = require('bcryptjs');
 const express = require('express');
+const mysql = require("mysql");
 
 const app = express();
 
+
+const database = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs-login'
+});
+
+// localhost/phpmyadmin to call database
+
+database.connect( (error) => {
+    if (error){
+        console.log(error)
+    } else {
+        console.log("MYSQL Connected...")
+    }
+});
+
 app.get("/", (req, res) => {
     res.send("<h1> Hello </h1>")
-})
+});
 
-app.listen(5005, () => {
-    console.log("Server started on Port 5005");
+
+app.listen(5006, () => {
+    console.log("Server started on Port 5006");
 })
