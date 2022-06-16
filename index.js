c8a8eb0e8745988394f49461ab6ebd47f7a37cf2
Merve Tuz <mervetuz11@gmail.com>
2022-06-17 01:46:59 +0200

    new_mysql_Login

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
-//we need to import Express that we just installed to make sure that we can start our server from nodejs
-const express = require('express');
-
-const session = require('express-session');
-
-const path = require('path');
-//import mysql
-const mysql = require('mysql');
-
-//Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
-//import dotenv
-const dotenv = require('dotenv');
-
-//config will read your .env file
-dotenv.config({path: './.env'})
-
-//start a server with app
+const express = require("express");
+const path = require("path")
+const mysql = require("mysql");
 const app = express();
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
-//put files like CSS or any JavaScript for the frontend
-//__dirname give you the directory of your file where you are
-const publicDirectory = path.join(__dirname,'./css')
-
-//Express is also using this directory
-app.use(express.static(publicDirectory));
-
-//Parse URL encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({extended: false}));
-
-//Parse JSON bodies(as sent by API clients)
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
+app.use(cookieParser());
+app.set('view engine', 'html');
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+const port = process.env.PORT ?? 5000;
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`Server listening at http://localhost:${port}`)
+    }
+});
+// Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
-//handlebars template
-app.set('view engine', 'hbs');
 
-//Define Routes
-app.use('/', require('./routes/pages-router'));
-app.use('/auth', require('./routes/register-router'));
-app.use('/login', require('./routes/login-router'));
-
-
-//you need to tell which port do you want to listen in order to start your first project
-app.listen(5000, () => {
-    console.log("Server started on Port 5000");
-})
--- routes/login-router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const registerController = require('../controllers/register-controller')
-const router = express.Router();
-
-//route for submit the Register form
-router.post('/login', registerController.register )
-
-
-module.exports = router;
